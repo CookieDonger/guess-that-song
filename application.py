ed12c3faa9d7ebe56154bdfe7a9996d23fce04fd
Derek Dong <122394745+CookieDonger@users.noreply.github.com>
2023-08-19 16:59:44 -0400

    Add files via upload

--- application.py
+++ application.py
@@ -13,6 +13,7 @@ application = Flask(__name__)
 application.secret_key = b'0d498dd9a4ea4b34f02cf616f5e0d2dc1ee60ac60526a6b4ed4ed9eec5bb04f4'
 application.config['SESSION_PERMANENT'] = False
 application.config['SESSION_TYPE'] = 'filesystem'
+application.config['SESSION_FILE_DIR'] = './.flask_session/'
 Session(application)
 
 load_dotenv()
@@ -20,19 +21,13 @@ load_dotenv()
 CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
 CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
 REDIRECT_URI = os.getenv('SPOTIPY_REDIRECT_URI')
+scope = "user-read-playback-state user-modify-playback-state user-read-currently-playing"
 
 
 @application.before_request
 def before_request():
-    # Hopefully instantiates for each user
-    session['playlists'] = session.get('playlists', [])
-    session['sp'] = session.get('sp', spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope="user-read-playback-state user-modify-playback-state user-read-currently-playing")))
-    session['playlist'] = session.get('playlist', '')
-    session['track'] = session.get('track', 0)
-    session['length'] = session.get('length', 0)
-    session['part'] = session.get('part', 0)
-    session['has_excerpt'] = session.get('has_excerpt', False)
-    session['error'] = session.get('error', '')
+    session['error'] = ''
+    return
 
 
 # Should do magic cache stuff, I'm not sure myself lol
@@ -44,8 +39,39 @@ def after_request(response):
     return response
 
 
+@application.route('/login', methods=['GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+
+@application.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+
 @application.route("/", methods=["GET", "POST"])
 def index():
+
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope=scope, cache_handler=cache_handler, show_dialog=True)
+
+    if request.args.get('code'):
+        auth_manager.get_access_token(request.args.get('code'))
+        session['playlists'] = []
+        session['playlist'] = ''
+        session['track'] = 0
+        session['length'] = 0
+        session['part'] = 0
+        session['has_excerpt'] = False
+        session['error'] = ''
+        return redirect('/')
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        auth_url = auth_manager.get_authorize_url()
+        return render_template('login.html', url=auth_url)
+
     # Just setting up the index page
     if request.method == "GET":
         return render_template("index.html", playlists=session['playlists'])
@@ -75,14 +101,16 @@ def index():
 # Option to remove playlists
 @application.route("/delete", methods=["POST"])
 def delete():
-    session['playlistremove'] = request.form.get("playlist")
-    session['playlists'].remove(session['playlistremove'])
+    session['playlists'].remove(request.form.get("playlist"))
     return redirect("/")
 
 
 # Here's where we manipulate the global variables
 @application.route("/guess", methods=["GET", "POST"])
 def guess():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+
     if request.method == "GET":
         # Not letting you go to the page if you have no playlists
         if len(session['playlists']) == 0:
@@ -92,12 +120,15 @@ def guess():
     if request.method == "POST":
         # You need to get an excerpt first, so I just redirect if there is no excerpt
         if session['has_excerpt'] is False:
-            return redirect("/guess")
+            return render_template('index.html', error='No excerpt yet')
+
+        sp = spotipy.Spotify(auth_manager=auth_manager)
+
         # For whole song
         if session['length'] == 0:
             # Making sure there's an active device
             try:
-                session['sp'].start_playback(device_id=session['sp'].devices()['devices'][0]['id'], context_uri=session['playlist'], offset={"position": session['track']})
+                sp.start_playback(device_id=sp.devices()['devices'][0]['id'], context_uri=session['playlist'], offset={"position": session['track']})
             except Exception:
                 session['error'] = "No active device found"
                 return render_template("index.html", error=session['error'], playlists=session['playlists'])
@@ -105,42 +136,54 @@ def guess():
         else:
             # Again making sure there's an active device
             try:
-                session['sp'].start_playback(device_id=session['sp'].devices()['devices'][0]['id'], context_uri=session['playlist'], offset={"position": session['track']}, position_ms=session['part'])
+                sp.start_playback(device_id=sp.devices()['devices'][0]['id'], context_uri=session['playlist'], offset={"position": session['track']}, position_ms=session['part'])
                 time.sleep(int(session['length']))
-                session['sp'].pause_playback()
+                sp.pause_playback()
             except Exception:
                 session['error'] = "No active device found"
                 return render_template("index.html", error=session['error'], playlists=session['playlists'])
-        return redirect('/guess')
+        return ('', 204)
 
 
 # Getting a new excerpt
 @application.route("/reload", methods=["POST"])
 def reload():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+
     # Checking if user specified length
     if request.form.get("length"):
         session['length'] = int(request.form.get("length"))
     # Random playlist
     rng = random.randint(0, len(session['playlists']) - 1)
     session['playlist'] = session['playlists'][rng]
+
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
     # Random track
-    session['total'] = session['sp'].playlist_items(session['playlist'], fields='total')
+    session['total'] = sp.playlist_items(session['playlist'], fields='total')
     session['total'] = session['total']['total']
     session['track'] = random.randint(0, session['total'] - 1)
     # Random part
-    session['duration'] = session['sp'].playlist_tracks(session['playlist'], fields='items', limit=1, offset=session['track'])
+    session['duration'] = sp.playlist_tracks(session['playlist'], fields='items', limit=1, offset=session['track'])
     session['duration'] = int(session['duration']['items'][0]['track']['duration_ms'])
     lengthms = int(session['length']) * 1000
     session['part'] = random.randint(0, session['duration'] - int(lengthms))
     session['has_excerpt'] = True
-    return render_template("guess.html")
+    return ('', 204)
 
 
 # This was a pain, I had to figure out the data that Spotify gave and figure out the lists and dictionaries to get the info
 # I did both song name and composer because some songs have the same name.
 @application.route("/reveal", methods=["POST"])
 def reveal():
-    session['song'] = session['sp'].playlist_tracks(session['playlist'], fields='items', limit=1, offset=session['track'])
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    if not session['track']:
+        return render_template('index.html', error='No excerpt yet')
+    session['song'] = sp.playlist_tracks(session['playlist'], fields='items', limit=1, offset=session['track'])
     session['songname'] = session['song']['items'][0]['track']['name']
     session['name'] = session['song']['items'][0]['track']['artists'][0]['name']
     return render_template("guess.html", songname=session['songname'], name=session['name'])
